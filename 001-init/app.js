4933a54fc776232b400f11365babfe345a5d5156
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-30 06:02:33 +0530

    setup routes

---
+++ 001-init/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+
+const path = require('path');
+const routes = require('./routes/index');
+
+// instantiate an application by calling express as 
+// a method and assigning to a variable app.
+const app = express();
+
+// once we have the application object - we need to tell it to listen to GET
+// requests on various paths.
+
+// we are going to let our app use that routes module that we just 
+// imported to do so.
