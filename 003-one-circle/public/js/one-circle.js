02233f9c957b002b825f6d21c017dee0046910c5
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-30 20:14:24 +0530

    added svg element

--- 003-one-circle/public/js/one-circle.js
+++ 003-one-circle/public/js/one-circle.js
@@ -1,1 +1,13 @@
-alert('Hello!');
+/*
+1. SVG
+2. cx, cy
+3. r
+4. fill - red
+*/
+
+const svg = d3.select("body")
+  .attr("margin",0)
+  .append('svg')
+  .attr('width',window.innerWidth)
+  .attr('height',window.innerHeight);
+
