a542cfea2c2eb6c1a8e242536f36b306137a5859
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-31 08:18:29 +0530

    adding line attributes

--- 004-one-line/public/js/one-line.js
+++ 004-one-line/public/js/one-line.js
@@ -8,9 +8,30 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 // set the background
-d3.select('body')
+const svg = d3.select('body')
   .attr('margin',0)
   .append('svg')
   .attr('width',width)
   .attr('height',height)
   .style('background-color','yellow');
+
+  // coordinates is an array of objects
+  const coordinates = [
+      {x:width/4, y : height/2},
+      {x:(width/4)*3, y:height/2}
+  ];
+
+  // we just want a straight line accross the screen
+  // begin: this is a line generator
+  const line = d3.line()
+                 .x(function(d){
+                     return d.x;
+                 })
+                 .y(d => d.y);
+
+  const path = svg.append('path')
+                  .datum(coordinates)
+                  .attr('d',line)
+                  .style('fill','none')
+                  .style("stroke","blue")
+                  .style("stroke-width","5px");
