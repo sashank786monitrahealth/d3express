3424c70e8de321087cabc63a5d7932e5482db9b5
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-09-01 17:25:27 +0530

    init

---
+++ 008-transitions-easing/public/js/one-circle.js
@@ -0,0 +1,20 @@
+/*
+1. SVG
+2. cx, cy
+3. r
+4. fill - red
+*/
+
+const svg = d3.select("body")
+  .attr("margin",0)
+  .append('svg')
+  .attr('width',window.innerWidth)
+  .attr('height',window.innerHeight);
+
+const circle = svg.append('circle')
+                .attr('cx',window.innerWidth/2)
+                .attr('cy',window.innerHeight/2)
+                .attr('r','100px')
+                .style('fill','purple')
+                .style('stroke','chartreuse')
+                .style('stroke-width',15); // 15 pixels
---
+++ 008-transitions-easing/routes/pages.js
@@ -0,0 +1,10 @@
+const pages = [
+    {location:'/', file:'index', page:'index'},
+    {location:'/one-circle', file:'one-circle'},
+    {location:'/one-line', file:'one-line'},
+    {location:'/one-box', file:'one-box'},
+    {location:'/confetti', file:'confetti'},
+    {location:'/stacked-circles', file:'stacked-circles'}
+]
+
+module.exports = pages
